f672970150e4d7e0598add02d820bc67f5eaea74
lyb <975965948@qq.com>
2020-12-03 03:10:17 +0800

    作业

--- Week02/errors.go
+++ Week02/errors.go
@@ -1,15 +1,3 @@
-# 第二周
-
-作业：1. dao 层中当遇到一个 sql.ErrNoRows 的时候，是否应该 Wrap 这个 error，抛给上层。为什么?
-
-答 应该。
-有两种情况，一种是我的sql 出现norows是正常情况，这种时候应该由上层决定怎么处理，如果这时候直接处理异常（打日志） 可能会污染日志
-第二种是异常情况，这种时候也应该由上层处理，因为只有上层的context 的价值是有用的，并且可以做降级处理。
-
-fake代码：
-
-
-```golang
 package week02
 
 import (
@@ -46,4 +34,3 @@ func BizSetUser(user User, id string) error {
     }
   }
 }
-```
